dac293531fcd217ebcd9b5b0816e0715bf35b462
Thembalethu Ndlovu <ST10099281@rcconnect.edu.za>
2024-10-24 18:34:10 +0200

    Added Models for LecturerClaim and FileUpload

---
+++ Models/FileUpload.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContractMonthlyClaimSystem.Models
+{
+    public class FileUpload
+    {
+        [Key]
+        public int FileId { get; set; }
+
+        [Required]
+        public int ClaimId { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public byte[] FileContent { get; set; }
+    }
+}
---
+++ Models/LecturerClaim.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContractMonthlyClaimSystem.Models
+{
+    public class LecturerClaim
+    {
+        [Key]
+        public int ClaimId { get; set; }
+
+        [Required]
+        public string LecturerName { get; set; }
+
+        [Required]
+        public int HoursWorked { get; set; }
+
+        [Required]
+        public decimal HourlyRate { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal TotalAmount => HoursWorked * HourlyRate;
+
+        public string AdditionalNotes { get; set; }
+
+        public string FileName { get; set; }
+
+        public ClaimStatus Status { get; set; } = ClaimStatus.Pending;
+
+        public DateTime SubmissionDate { get; set; } = DateTime.Now;
+    }
+
+    public enum ClaimStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+}
