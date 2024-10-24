92b9b7cd8f6f66fbd9e3a89ec096743c0b795586
Thembalethu Ndlovu <ST10099281@rcconnect.edu.za>
2024-10-24 19:05:12 +0200

    Implemented Lecturer Claim Submission Form with backend validation

---
+++ Controllers/LecturerClaimsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContractMonthlyClaimSystem.Data;
+using ContractMonthlyClaimSystem.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    public class LecturerClaimsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LecturerClaimsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LecturerClaims/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LecturerClaims/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(LecturerClaim claim)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(claim);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(claim);
+        }
+
+        // GET: LecturerClaims
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LecturerClaims.ToListAsync());
+        }
+    }
+}
---
+++ Views/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContractMonthlyClaimSystem.Views
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
